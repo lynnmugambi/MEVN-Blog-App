87043570a0d2c567bdb63d1d47c3caf3234686b5
Lynn Mugambi <lynnmugambi@Lynns-MacBook-Pro.local>
2019-07-15 16:14:48 +0300

    created posts component

---
+++ client/src/components/Posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="posts">
+    <h1> Posts </h1>
+    This file will list all the posts.
+  <div v-for="(post, index) in posts" :key="index">
+      <p>
+        <span><b>{{ post.title }}</b></span><br />
+        <span>{{ post.description }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PostsService from '@/controllers/PostsController'
+export default {
+  name: 'posts',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
+  }
+}
+</script>
